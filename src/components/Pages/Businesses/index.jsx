ee8ce292162ff2b6ae12609d3a213317adc0dd47
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-10-29 15:11:04 +0100

    updated business index

--- src/components/Pages/Businesses/index.jsx
+++ src/components/Pages/Businesses/index.jsx
@@ -3,9 +3,8 @@ function Businesses() {
     <>
       <h2>Businesses</h2>
       <p>
-        A table of all the businesses we serve, maybe with stats like number of
-        students and whether or not the Business' subscription is active. This
-        should only show if one of us is signed in.
+        A table of all the businesses the user is a member of (either as an
+        owner, teacher or student).
       </p>
     </>
   );
